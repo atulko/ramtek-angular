3ef7e080e84fb706190c65533eb30c33a35caf6c
ATUL <atul.koypare@gmail.com>
2021-10-10 21:40:02 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
+import { PortfolioComponent } from './components/portfolio/portfolio.component';
+import { FeedbackComponent } from './components/feedback/feedback.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { TestimonialComponent } from './components/testimonial/testimonial.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    BlogComponent,
+    ContactComponent,
+    PortfolioComponent,
+    FeedbackComponent,
+    FooterComponent,
+    TestimonialComponent
   ],
   imports: [
     BrowserModule,
